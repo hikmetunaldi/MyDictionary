e6a3a23d4b51df68ecf567183acdf6474cabe0f9
ehikm <ehikm@HIKMET>
2023-02-26 17:53:57 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyListAndDictionary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyListAndDictionary<TKey,TValue>
+    {
+        TKey[] Id;
+        TValue[] Name;
+        TKey[] tempId;
+        TValue[] tempName;
+
+        public MyListAndDictionary()
+        {
+            Id = new TKey[0];
+            Name = new TValue[0];
+            
+        }
+
+        public void Add(TKey id , TValue name)
+        {
+            tempId = Id;
+            tempName = Name;
+
+            Id = new TKey[Id.Length + 1];
+            Name = new TValue[Name.Length + 1];
+
+            for (int i = 0; i < tempId.Length; i++)
+            {
+                Id[i] = tempId[i];
+
+                Name[i] = name;
+
+            }
+
+            Id[Id.Length - 1] = id;
+            Name[Id.Length - 1] = name;
+
+
+        }
+        public TKey[] ids
+        {
+            get { return Id; }
+        }
+
+        public TValue[] names
+        { 
+            get { return Name; } 
+        }
+
+        public int NameLenght 
+        { 
+            get { return Name.Length; } 
+        }
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyListAndDictionary<int , string> myListAndDictionary = new MyListAndDictionary<int , string>();
+
+            myListAndDictionary.Add(1235, "Hikmet");
+            myListAndDictionary.Add(5654, "Ali");
+            myListAndDictionary.Add(5465, "Selin");
+
+            for (int i = 0; i < myListAndDictionary.NameLenght; i++)
+            {
+                Console.WriteLine("Kisi Id :"+ myListAndDictionary.ids[i]+"      "+"Kisi Name" + myListAndDictionary.names[i]);
+            }
+
+            Console.WriteLine("Kisi Sayisi:  "+ myListAndDictionary.NameLenght);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
